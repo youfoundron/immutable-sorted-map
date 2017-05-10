8212a8d0033c2cc67dc508d53e727bede176fddf
Ron Gierlach <rongierlach@gmail.com>
2017-05-10 14:37:43 -0500

    build: create webpack config

---
+++ webpack.config.babel.js
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import { optimize } from 'webpack'
+import makeRule from 'webpack-make-rule'
+
+export default {
+  entry: resolve(__dirname, './src/index.js'),
+  output: {
+    path: resolve(__dirname, './lib'),
+    filename: 'index.js',
+    library: 'ImmutableTrees',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [ makeRule('js', 'babel-loader') ]
+  },
+  externals: {
+    'immutable': {
+      commonjs: 'immutable',
+      commonjs2: 'immutable',
+      amd: 'immutable',
+      root: 'Immutable'
+    }
+  },
+  plugins: [
+    new optimize.UgflifyJsPlugin({
+      comments: false
+    })
+  ]
+}
