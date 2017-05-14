3d823d3582c55af568f1a86afb06f7330d9ce6a8
Ron Gierlach <rongierlach@gmail.com>
2017-05-14 00:14:54 -0500

    feat: allow passing of a callback ot getMinNode and getMaxNode

--- src/util/getMaxNode.js
+++ src/util/getMaxNode.js
@@ -1,6 +1,7 @@
 import isMap from './isMap'
+import identityFunc from './identityFunc'
 
-export default function getMaxNode (node = this.root) {
+export default function getMaxNode (node = this.root, callback = identityFunc) {
   const rightChild = node.get('right')
-  return isMap(rightChild) ? this.getMaxNode(rightChild) : node
+  return isMap(rightChild) ? this.getMaxNode(rightChild) : callback(node)
 }
--- src/util/getMinNode.js
+++ src/util/getMinNode.js
@@ -1,6 +1,7 @@
 import isMap from './isMap'
+import identityFunc from './identityFunc'
 
-export default function getMinNode (node = this.root) {
+export default function getMinNode (node = this.root, callback = identityFunc) {
   const leftChild = node.get('left')
-  return isMap(leftChild) ? this.getMinNode(leftChild) : node
+  return isMap(leftChild) ? this.getMinNode(leftChild) : callback(node)
 }
