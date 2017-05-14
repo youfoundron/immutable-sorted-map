8819435095b457897808bfffeec3b4f464649a51
Ron Gierlach <rongierlach@gmail.com>
2017-05-13 23:32:10 -0500

    refactor: refactor Tree#get

--- src/api/readingValues/get.js
+++ src/api/readingValues/get.js
@@ -1,17 +1,3 @@
 export default function (key) {
-  let found = false
-  let current = this.root
-
-  while (!found && current) {
-    const currentKey = current.get('key')
-    if (key < currentKey) {
-      current = current.get('left')
-    } else if (key > currentKey) {
-      current = current.get('right')
-    } else {
-      found = true
-    }
-  }
-
-  return current && current.get('value')
+  return this.search(key, node => node && node.get('value'))
 }
