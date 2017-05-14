896c9e5e472a5d37a991f2375359e7fd41a8cafb
Ron Gierlach <rongierlach@gmail.com>
2017-05-13 22:31:51 -0500

    feat: create file with some named exports for util module

---
+++ src/util/index.js
@@ -0,0 +1,9 @@
+import isMap from './isMap'
+import calculateSize from './calculateSize'
+import traverseByDepth from './traverseByDepth'
+
+export {
+  isMap,
+  calculateSize,
+  traverseByDepth
+}
