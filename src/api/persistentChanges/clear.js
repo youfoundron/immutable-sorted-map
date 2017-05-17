36e3c3a1b94dbbac6e03b03a14d4966b02f736d2
Ron Gierlach <rongierlach@gmail.com>
2017-05-17 00:25:33 -0500

    feat: write Tree#clear

---
+++ src/api/persistentChanges/clear.js
@@ -0,0 +1,3 @@
+export default function () {
+  return this.klass(this.root.clear())
+}
--- src/api/persistentChanges/index.js
+++ src/api/persistentChanges/index.js
@@ -1,9 +1,11 @@
 import set from './set'
 import _delete from './delete'
 import deleteAll from './deleteAll'
+import clear from './clear'
 
 export default {
   set,
   delete: _delete,
-  deleteAll
+  deleteAll,
+  clear
 }
