01b710830427fb5ec855c38901ed24cc4e40183c
Ron Gierlach <rongierlach@gmail.com>
2017-05-17 22:52:14 -0500

    style: updater -> merger

--- src/api/persistentChanges/mergeWith.js
+++ src/api/persistentChanges/mergeWith.js
@@ -1,13 +1,13 @@
 import identityFunc from '../../util/identityFunc'
 
-export default function (updater = identityFunc, ...trees) {
+export default function (merger = identityFunc, ...trees) {
   return trees.reduce((resultTree, currTree) => {
     currTree.traverseByDepth(node => {
       const key = node.get('key')
       const newVal = node.get('value')
       if (resultTree.has(key)) {
         const oldVal = resultTree.get(key)
-        resultTree = resultTree.set(key, updater(oldVal, newVal))
+        resultTree = resultTree.set(key, merger(oldVal, newVal))
       } else {
         resultTree = resultTree.set(key, newVal)
       }
