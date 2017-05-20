5566966d774b59f3ea50f55527818aaed450ad0f
Ron Gierlach <rongierlach@gmail.com>
2017-05-20 16:10:11 -0500

    refactor: remove unecessary if/else from util#isTreeMap

--- src/util/isTreeMap.js
+++ src/util/isTreeMap.js
@@ -1,6 +1,9 @@
 import isMap from './isMap'
 
 export default function (data) {
-  if (isMap(data) && data.has('key') && data.has('value')) return true
-  return false
+  return (
+    isMap(data) &&
+    data.has('key') &&
+    data.has('value')
+  )
 }
