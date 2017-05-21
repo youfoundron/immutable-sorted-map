227858f23b5c82648683465a1d7bd0119c565a34
Ron Gierlach <rongierlach@gmail.com>
2017-05-20 19:07:43 -0500

    test: add iterators tests to main test file

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ import readingValues from './readingValues'
 import readingDeepValues from './readingDeepValues'
 import persistentChanges from './persistentChanges'
 import deepPersistentChanges from './deepPersistentChanges'
+import iterators from './iterators'
 import sideEffects from './sideEffects'
 
 describe('SortedMap', () => {
@@ -13,5 +14,6 @@ describe('SortedMap', () => {
   describe('Reading deep values', readingDeepValues)
   describe('Persistent changese', persistentChanges)
   describe('Deep persistent changes', deepPersistentChanges)
+  describe('Iterators', iterators)
   describe('Side effects', sideEffects)
 })
---
+++ test/iterators/index.js
@@ -0,0 +1,9 @@
+import keys from './keys'
+import values from './values'
+import entries from './entries'
+
+export default () => {
+  it('#keys', keys)
+  it('#values', values)
+  it('#entries', entries)
+}
