685c0beecb754a870416217665c9e4b8e40346a5
Ron Gierlach <rongierlach@gmail.com>
2017-05-20 21:04:54 -0500

    feat(api): write SortedMap#toJS

---
+++ src/api/conversionToJavascriptTypes/toJS.js
@@ -0,0 +1,11 @@
+import { isImmutable } from 'immutable'
+
+export default function () {
+  const result = {}
+  this.traverseByDepth(node => {
+    const key = node.get('key')
+    const value = node.get('value')
+    result[key] = isImmutable(value) ? value.toJS() : value
+  })
+  return result
+}
